77effcab7fe8ceb37a59d9d15ac71958af58254b
Lê Trần Tiến Phát <tienphat0707@gmail.com>
2023-04-25 10:27:44 +0700

    Update image

--- src/common/components/ProjectShowcase/ProjectCard/index.tsx
+++ src/common/components/ProjectShowcase/ProjectCard/index.tsx
@@ -16,7 +16,7 @@ const ProjectCard = ({ item }: Props, ref: any) => {
   const appearAnimation = {
     transform: isInView ? "none" : "translateX(-200px)",
     opacity: isInView ? 1 : 0,
-    transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+    transition: "all 0.2s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
   }
 
   return (
